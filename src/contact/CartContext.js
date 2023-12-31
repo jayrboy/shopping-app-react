27b27003f1ba1ff4d04b4d6ec00719eef3a22d12
jayrboy <j.jayox_@hotmail.com>
2023-12-31 14:56:18 +0700

    Add State CartContext.js
    Add State CartContext.js

--- src/contact/CartContext.js
+++ src/contact/CartContext.js
@@ -1,7 +1,12 @@
 import { createContext, useContext } from "react";
+import products from "../data/products";
 
-// create context
 const CartContext = createContext();
+const initState = {
+  products: products,
+  total: 0,
+  amount: 0,
+};
 
 // การนำเอา context ไปใช้งานด้านนอก
 export const useCart = () => {
