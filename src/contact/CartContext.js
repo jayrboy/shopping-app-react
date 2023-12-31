f93b63bbe552422540498d50e53b512b401eb0f9
jayrboy <j.jayox_@hotmail.com>
2023-12-31 15:33:48 +0700

    Add Provider CartContext.js index.js
    Add Provider CartContext.js index.js

--- src/contact/CartContext.js
+++ src/contact/CartContext.js
@@ -1,5 +1,6 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useReducer } from "react";
 import products from "../data/products";
+import cartReducer from "../reducer/cartReducer";
 
 const CartContext = createContext();
 const initState = {
@@ -8,7 +9,15 @@ const initState = {
   amount: 0,
 };
 
+export const CartProvider = ({ children }) => {
+  // eslint-disable-next-line
+  const [state, dispatch] = useReducer(cartReducer, initState);
+  return (
+    <CartContext.Provider value={{ ...state }}>{children}</CartContext.Provider>
+  );
+};
+
 // การนำเอา context ไปใช้งานด้านนอก
 export const useCart = () => {
-  return useContext(useCart);
+  return useContext(CartContext);
 };
